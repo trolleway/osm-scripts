0330cf841aaffd5f399cb87a76b7abd1cb5f677d
trolleway <trolleway@yandex.ru>
2014-03-29 05:33:53 +0300

    Add tags landuse=residential and source=highway buffer

--- landuse_by_buffers/main.py
+++ landuse_by_buffers/main.py
@@ -2,7 +2,7 @@ import os
 import psycopg2
 
 #Take an roads.geojson, and make a many polygons beetwen roads in osm format.
-#Open OSM file in JOSM, add a "landuse=residential", and you get a fancy medium-zoom landuse map
+#Open OSM file in JOSM, and you get a fancy medium-zoom landuse map
 
 
 def process():
@@ -21,6 +21,7 @@ def process():
 	sql ='''
 	DROP TABLE IF EXISTS roads;
 	DROP TABLE IF EXISTS areas;
+	DROP TABLE IF EXISTS areas_singlegeom;
 	
 	'''
 
@@ -38,28 +39,35 @@ def process():
 	sql='''
 	CREATE TABLE areas AS
 	SELECT 
-	1 AS id,
+	1 AS id,	
 	ST_Difference(
 		ST_ConvexHull(ST_Collect(wkb_geometry)),
 		ST_Buffer(ST_Collect(wkb_geometry),10)
 		)
 		AS poly
-	FROM roads;
 	
+	FROM roads;
+
+	CREATE TABLE areas_singlegeom AS
+	SELECT
+	ST_GeometryN(poly,generate_series(1,ST_NumGeometries(poly))) AS geom,
+	'residential' AS landuse,
+	'highway buffers' AS source
+	FROM areas;
 	'''
 	cur.execute(sql)
 	conn.commit()
 
 	os.system('''
-	ogr2ogr -overwrite data PG:"dbname='residential' host='localhost' port='5432' user='user' password='user'"  "areas" -nlt MULTIPOLYGON -s_srs EPSG:3857 -t_srs EPSG:4326
+	ogr2ogr -overwrite data PG:"dbname='residential' host='localhost' port='5432' user='user' password='user'"  "areas_singlegeom" -nlt MULTIPOLYGON -s_srs EPSG:3857 -t_srs EPSG:4326
 	''')
 
 	os.system('''
-	rm areas.osm
+	rm areas_singlegeom.osm
 	''')
 
 	os.system('''
-python ogr2osm/ogr2osm.py data/areas.shp 
+python ogr2osm/ogr2osm.py data/areas_singlegeom.shp
 	''')
 
 if __name__ == '__main__':
