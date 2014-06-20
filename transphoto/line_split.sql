c66514aa134ff1a7f6303c3da02fb7ace3daac4c
trolleway <trolleway@yandex.ru>
2014-06-20 09:29:46 +0400

    Update line_split.sql

--- transphoto/line_split.sql
+++ transphoto/line_split.sql
@@ -1,14 +1,44 @@
 -- Разбивка линий на сегменты
 -- Проверено на типе LINESTRING
 
-SELECT (ST_MakeLine(start_point,end_point)) as geom, row_number() OVER () AS id,
-round(degrees(ST_Azimuth(start_point,end_point))) AS azimuth
+DROP VIEW IF EXISTS sunlines_all;
+CREATE OR REPLACE VIEW sunlines_all AS
+SELECT (ST_MakeLine(start_point,end_point)) as wkb_geometry, row_number() OVER () AS id,
+degrees(ST_Azimuth(start_point,end_point)) AS azimuth
   FROM
 (
     SELECT 
-        ST_Pointn(geom, generate_series(1, ST_NumPoints(geom)-1)) as start_point, 
-        ST_Pointn(geom, generate_series(2, ST_NumPoints(geom))) as end_point
+        ST_Pointn(wkb_geometry, generate_series(1, ST_NumPoints(wkb_geometry)-1)) as start_point, 
+        ST_Pointn(wkb_geometry, generate_series(2, ST_NumPoints(wkb_geometry))) as end_point
     FROM (
-        SELECT  geom FROM lines
+        SELECT  wkb_geometry FROM lines
         ) as line
-) as tmp;   
+) as tmp;       
+    
+DROP VIEW IF EXISTS sunlines_1000;
+CREATE OR REPLACE VIEW sunlines_1000 AS
+SELECT * 
+FROM sunlines_all
+WHERE
+azimuth BETWEEN 90 AND 105;
+
+DROP VIEW IF EXISTS sunlines_1300;
+CREATE OR REPLACE VIEW sunlines_1300 AS
+SELECT * 
+FROM sunlines_all
+WHERE
+azimuth BETWEEN 160 AND 170;
+
+DROP VIEW IF EXISTS sunlines_1500;
+CREATE OR REPLACE VIEW sunlines_1500 AS
+SELECT * 
+FROM sunlines_all
+WHERE
+azimuth BETWEEN 180 AND 190;
+
+DROP VIEW IF EXISTS sunlines_1900;
+CREATE OR REPLACE VIEW sunlines_1900 AS
+SELECT * 
+FROM sunlines_all
+WHERE
+azimuth BETWEEN 260 AND 270;
